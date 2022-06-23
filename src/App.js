86087f62cbf0cb379c4415f3f1f9900f96fb9304
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-06-23 15:40:21 -0400

    Add styling for dark mode

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="nav"><h1>Kurtis Grant</h1></div>
-      Content
+      <div className="header">
+        <h1 className="heading">Kurtis Grant</h1>
+        <h2 className="sub-heading">Full Stack Developer from Ottawa, Canada</h2>
+        </div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ea maxime, voluptatum voluptates dignissimos assumenda maiores, aspernatur fugit officiis commodi ullam id perferendis temporibus mollitia quos ipsa molestias enim. Doloremque maxime illum impedit exercitationem, consequuntur inventore ex architecto ullam ipsam voluptates fugit pariatur esse modi omnis aspernatur at magnam mollitia excepturi nobis autem consectetur. <a href="#">This is a link</a> Placeat odit atque quam qui eligendi illum rem, illo vel. Officia animi labore quo ex adipisci deleniti? Voluptates nam amet in sapiente quibusdam vero facilis eaque! Illo id cumque aut ea voluptates autem ut nobis, quod quasi placeat ducimus aliquid exercitationem veritatis dignissimos nisi enim asperiores.
+      
     </div>
   );
 }
