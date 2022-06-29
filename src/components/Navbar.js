7e6cc8f936875de3bb28b3ab1d6f2e2c7814c451
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-06-29 07:42:48 -0400

    Implement navlinks from react router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "./Navbar.css"
 
 function Navbar() {
   return (
     <div className="navbar">
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
       </nav>
     </div>
   )
