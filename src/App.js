e47bc0ac9071aa889d670700088b79bd5ec5201e
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-07-04 15:23:06 -0400

    Add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Outlet } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="container">
         <Navbar />
         <Outlet />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <hr />
+      <div className="footer-cols">
+        <div className="footer-row">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/projects">Projects</NavLink>
+        </div>
+        <div className="footer-row">
+          <a href="https://github.com/kurtisgrant/" target="_blank">GitHub</a>
+          <a href="https://linkedin.com/in/kurtis-grant/" target="_blank">LinkedIn</a>
+        </div>
+      </div>
+      <div className="footer-end">Website designed by Kurtis Grant © 2022</div>
+    </div>
+  );
+}
+
+export default Footer;
