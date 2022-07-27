dcc362441b9b914bd33ce602bbbb8a54a3cb3eb8
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-07-27 16:45:21 -0400

    Update bio in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,9 +16,24 @@ function Home() {
           <img src={profileImg} alt="Profile photo" />
         </div>
       </div>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod deleniti id minima magnam aspernatur, nam accusantium non doloribus quo aperiam perspiciatis quidem, officiis numquam soluta voluptate! Accusamus perspiciatis soluta nesciunt ipsam autem odit cumque dolore dolorem corrupti iusto.</p>
-      <p>Cum neque inventore in minus dolor nemo, nulla porro. A animi, minima optio ea rem odio blanditiis sit suscipit exercitationem quo voluptates dicta omnis dolore ullam sunt totam tenetur amet perspiciatis accusantium iure corporis quia eos repellat! Aliquid omnis voluptas suscipit dignissimos ducimus quibusdam aliquam earum modi rerum veritatis animi, similique quae ut laudantium rem odio eveniet sint inventore quis doloribus labore.</p>
-      <p>Deleniti autem dolore aperiam neque, facilis dolores debitis ea. Vitae vero beatae, quaerat omnis cum nobis architecto nemo placeat voluptatibus. Aliquid atque soluta optio consequuntur perspiciatis laboriosam deleniti cum ratione earum necessitatibus possimus pariatur, iusto dolor eius autem dignissimos accusantium aspernatur libero.</p>
+      <div className="bio">
+
+        <p>Hi there, I’m Kurtis! I’m an avid problem solver with endless curiosity and a broad range of interests, including business, economics, psychology and computer science. I’ve worked with many small businesses and love building creative solutions that solve their problems while considering design, user experience and accessibility.</p>
+
+        <p>My software development journey began after spending two years in my small rural hometown of Bancroft, building simple websites and setting up SAAS products for several non-profits and small businesses. During this time, I discovered my love of diagnosing and coming up with solutions to problems organizations face when they don’t have the right digital tools to do their jobs efficiently.</p>
+
+        <p>In 2019, I began experimenting with a no-code online database called Knack, which I used to help a few non-profits replace the frustrating, messy spreadsheet systems they had used for years. This work ultimately led to my wanting to build more robust applications better tailored to the needs of the businesses I serve.</p>
+
+        <p>In the years following, I studied web development independently for two years, gaining a solid understanding of HTML, CSS and JavaScript through online courses and the highly regarded “Eloquent JavaScript” textbook. After this, I completed Harvard University’s “CS50 - Intro to Computer Science” course, cementing my understanding of programming principles and introducing me to the Python programming language.</p>
+
+        <p>As I was preparing to begin my job search, I came across an opportunity to attend Canada’s Lighthouse Labs, a rigorous three-month full-time web development bootcamp. I took this opportunity and gained additional experience building numerous projects with JavaScript, React and Ruby on Rails. I also got valuable team collaboration experience with Git, something I previously lacked as a self-taught developer.</p>
+
+        <p>At Lighthouse Labs, I pushed myself to go above and beyond the course’s expectations, completing additional course work, reading independently about the intricacies of the technologies we used and taking advantage of the expertise of my lecturers by staying late to discuss advanced topics.</p>
+
+        <p>My next immediate goal is to find a role within a company where I can continue to apply myself, be challenged daily and continue learning at an accelerated pace.</p>
+
+      </div>
+
     </main>
 
 
