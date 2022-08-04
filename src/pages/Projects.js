a33b1e9d0226f486b552631044324552a898ce06
Kurtis Grant <kurtis.b.grant@gmail.com>
2022-08-04 18:01:54 -0400

    Replace placeholder projects with actual ones

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,9 +7,9 @@ function Projects() {
     <main className="projects-page">
       <h1 className="heading">Projects</h1>
       <div className="project-container">
-        <ProjectCard title="Test" slug="test-slug">This is some description of the "Test" Project.</ProjectCard>
-        <ProjectCard title="Test" slug="test-slug">This is some description of the "Test" Project.</ProjectCard>
-        <ProjectCard title="Test" slug="test-slug">This is some description of the "Test" Project.</ProjectCard>
+        <ProjectCard title="Smart Money" external slug="https://github.com/kurtisgrant/smart-money">is a teaching tool to introduce youth to the concept of investing, inflation and the benefits of leveraging compound interest. (React, NodeJS, Express, PostgreSQL, Socket.io)</ProjectCard>
+        <ProjectCard title="Thordle" external slug="https://github.com/kurtisgrant/thordle">is an adaptation of the word puzzle game Wordle. In addition as core gameplay logic, Thordle features complex conditional rendering, state management and CSS animations. It also supports light/dark mode and local storage of user stats. (React, Styled-Components, Framer Motion)</ProjectCard>
+        <ProjectCard title="Interview Scheduler" external slug="https://github.com/kurtisgrant/scheduler">is a simple application which allows you to create, edit and delete interview appointments.</ProjectCard>
       </div>
     </main>
   );
